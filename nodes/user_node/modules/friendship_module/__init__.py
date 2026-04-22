6d41cf5c16b94cfa68f0879510435f5fb0997556
Yarbick <yarbilovilyas@gmail.com>
2026-04-22 21:08:05 +0300

    Добавлены обработчики для просмотра отношений пользователя с другими пользователями
     * Добавлены обработчики для просмотра отношений пользователя с другими пользователями:
      (далее путь nodes/user_node/modules/friendship_module будет представлен как friendship_module)
    
      friendship_module/__init__.py:
        1. Добавлена инициализация обработчиков
    
      friendship_module/routes.py:
        1. Добавлен макет-обработчик для просмотра пользователей по статусу отношений
        2. Добавлен обработчик для просмотра друзей
        3. Добавлен обработчик для просмотра ожидаемых друзей
        4. Добавлены обработчик для просмотра заблокированных пользователей
    
      friendship_module/templates/friendships.html:
        1. Добавлена главная страница для просмотра отношений с другими пользователями
    
      friendship_module/static/css/friendships.css:
        1. Добавлены стили для главной страницы модуля Friendship
    
      shared_templates/base.html:
        1. Добавлен элемент в навигационную панель для перехода на главную страницу модуля Friendship

--- nodes/user_node/modules/friendship_module/__init__.py
+++ nodes/user_node/modules/friendship_module/__init__.py
@@ -2,5 +2,7 @@
 
 # Создание blueprint
 from .blueprint import bp
+# Подключение обработчиков
+from .routes import bp
 # Создание API
 from .api import api_bp
--- nodes/user_node/modules/friendship_module/routes.py
+++ nodes/user_node/modules/friendship_module/routes.py
@@ -4,18 +4,69 @@
 from flask import render_template, url_for, redirect, request, flash
 from flask_login import current_user, login_required
 
-# Безопасность
-from security.csrf import create_csrf_request_session
-
 # Подключение к модулю
 from .blueprint import bp
-
-# Настройки модуля
-from .config import Config
 
 # Работа с REST API
 import requests
 
-# Формы
-from .forms.profile_edit import ProfileEditForm
-from .forms.user_delete import UserDeleteForm
+
+def create_friendship_view_handler(friendship_status: str) -> str:
+    """Создание обработчика для просмотра связей пользователя с другими пользователями по статусу отношений"""
+
+    # Дополнительные данные для отображения страницы
+    title: str = friendship_status.lower().capitalize()
+
+    # Получение связей с пользователями через REST API
+    # Подготовка данных
+    server_address = f"{"https" if request.is_secure else "http"}://{request.host}"
+    json_params = {
+        "status": friendship_status.lower()
+    }
+    # Запрос
+    response = requests.get(
+        f"{server_address}/api/v1/users/{current_user.id}/friendships",
+        json=json_params
+    )
+
+    # Обработка запроса
+    if response:
+        # Получение связей с другими пользователями
+        user_friendships: dict = response.json()["friendships"]
+
+        # Отображение страницы (GET)
+        return render_template(
+            "friendships.html",
+            title=title,
+            user_friendships=user_friendships
+        )
+    # Отображение страницы в случае ошибки (GET)
+    return render_template(
+        "friendships.html",
+        title=title,
+        error_message=response.reason
+    )
+
+
+@bp.route("/view/accepted", methods=["GET"])
+@login_required
+def view_accepted():
+    """Просмотр друзей пользователя"""
+
+    return create_friendship_view_handler("accepted")
+
+
+@bp.route("/view/pending", methods=["GET"])
+@login_required
+def view_pending():
+    """Просмотр аккаунтов с приглашением в друзья от пользователя"""
+
+    return create_friendship_view_handler("pending")
+
+
+@bp.route("/view/blocked", methods=["GET"])
+@login_required
+def view_blocked():
+    """Просмотр заблокированных аккаунтов пользователем"""
+
+    return create_friendship_view_handler("blocked")
