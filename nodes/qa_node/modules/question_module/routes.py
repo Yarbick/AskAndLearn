7989dd3219372928ebd7c29c539bf363de8c7b13
Yarbick <yarbilovilyas@gmail.com>
2026-05-01 00:12:37 +0300

    Добавлена возможность читать, создавать, изменять, удалять комментарии, реализовано отображение времени
     * Добавлена возможность читать, создавать, изменять, удалять комментарии:
       (далее путь nodes/qa_node/modules будет представлен как modules)
    
       modules/comment_module/routes.py:
         1. Создан обработчик для редактирования комментария
         2. Создан обработчик для удаления комментария
    
       modules/comment_module/forms/comment/edit.py
         1. Создана форма для изменения комментария
    
       modules/comment_module/templates/comment/edit.html:
         1. Создан шаблон для изменения комментария
    
       modules/question_module/routes.py:
         1. В обработчик question_view добавлен просмотр комментариев
         2. В обработчик question_view добавлена возможность создать комментарий
    
       modules/question_module/templates/question/view.html:
         1. Добавлен просмотр комментариев
         2. Добавлена возможность создать комментарий
    
       modules/question_module/forms/comment/create.py:
         1. Реализована форма для создания комментариев
    
     * Реализовано отображение времени:
       shared_static/css/main.css:
         1. Добавлен стиль для мелкого текста
    
       nodes/qa_node/modules/question_module/templates/question/view.html:
         1. Добавлено отображение времени создания вопроса/комментария

--- nodes/qa_node/modules/question_module/routes.py
+++ nodes/qa_node/modules/question_module/routes.py
@@ -23,10 +23,12 @@ import requests
 # Работа с файлами
 from os import remove as remove_file
 
-# Формы
-from .forms.create import QuestionCreateForm
-from .forms.edit import QuestionEditForm
-from .forms.search import QuestionSearchForm
+# Формы для вопросов
+from .forms.question.create import QuestionCreateForm
+from .forms.question.edit import QuestionEditForm
+from .forms.question.search import QuestionSearchForm
+# Формы для комментариев
+from .forms.comment.create import CommentCreateForm
 
 
 @bp.route("", methods=["GET"])
@@ -88,23 +90,54 @@ def home():
     )
 
 
-@bp.route("/<int:question_id>/view", methods=["GET"])
+@bp.route("/<int:question_id>/view", methods=["GET", "POST"])
 def view(question_id: int):
     """Просмотр вопроса"""
 
     # Подготовка данных для REST API
     server_address = f"{request.scheme}://{request.host}"
+    request_session: requests.Session = create_csrf_request_session(server_address)
+
+    # Форма для создания комментария
+    comment_create_form = CommentCreateForm()
+
+    # Создание комментария (POST)
+    if comment_create_form.validate_on_submit():
+        # Создание комментария через REST API
+        # Подготовка данных
+        json_params = {
+            "content": comment_create_form.content.data,
+            "creator_id": current_user.id,
+            "question_id": question_id
+        }
+        # Запрос
+        response: requests.Response = request_session.post(
+            f"{server_address}/api/v1/comments",
+            json=json_params,
+            cookies=request.cookies
+        )
+
+        # Обработка запроса
+        if response:
+            # Вывод сообщения
+            flash("The comment has been created", "info")
+        else:
+            # Обработка ошибок
+            ResponseErrorHandler.flash_reason_message(response)
+
+        # Обновление страницы
+        return redirect(url_for("question.view", question_id=question_id))
 
     # Получение данных о вопросе через REST API
     # Запрос
-    response = requests.get(f"{server_address}/api/v1/questions/{question_id}")
+    response: requests.Response = request_session.get(f"{server_address}/api/v1/questions/{question_id}")
 
     # Обработка запроса
     if response:
         question = response.json()["question"]
 
+        # Запись посещения вопроса в сессию
         if current_user.is_authenticated:
-            # Запись посещения вопроса в сессию
             # Получение cookie-сессии последних вопросов
             if not flask_session.get("last_questions", None):  # Создаём список, если сессии не существует
                 flask_session["last_questions"] = []
@@ -122,16 +155,58 @@ def view(question_id: int):
 
     # Получение данных об авторе вопроса
     if question and question["creator_id"]:
-        response = requests.get(f"{server_address}/api/v1/users/{question["creator_id"]}")
+        response: requests.Response = request_session.get(f"{server_address}/api/v1/users/{question["creator_id"]}")
         question_creator = response.json()["user"] if response else None
     else:
         question_creator = None
 
+    # Получение данных о комментариях на вопрос
+    comments: list[dict] = []
+    if question:
+        # Получение комментариев через REST API
+        # Подготовка данных
+        json_params = {
+            "sort_mode": "new",
+            "search": str(question_id),
+            "search_mode": "question"
+        }
+        # Запрос
+        response: requests.Response = request_session.get(
+            f"{server_address}/api/v1/comments",
+            json=json_params
+        )
+
+        # Обработка запроса
+        if response:
+            # Получение комментариев
+            comments: list[dict] = response.json()["comments"]
+
+            # Получение информации об авторах комментариев
+            creators_cash = {}  # Сохраняем пользователей, чтобы не повторять запросы
+            for comment in comments:
+                if not comment["creator_id"] and comment["creator_id"] != 0:
+                    comment["creator"] = None
+                elif creators_cash.get(comment["creator_id"]):
+                    comment["creator"] = creators_cash.get(comment["creator_id"])
+                else:
+                    # Получение пользователя через REST API
+                    # Запрос
+                    response: requests.Response = request_session.get(
+                        f"{server_address}/api/v1/users/{comment["creator_id"]}"
+                    )
+
+                    # Обработка запроса
+                    creator = response.json()["user"] if response else None
+                    creators_cash[comment["creator_id"]] = creator
+                    comment["creator"] = creator
+
     # Отображение страницы (GET)
     return render_template(
         "question/view.html",
+        comment_create_form=comment_create_form,
         question=question,
-        question_creator=question_creator
+        question_creator=question_creator,
+        comments=comments
     )
 
 
@@ -189,6 +264,9 @@ def create():
             # Сохранение изображения
             if image:
                 image.save(f"{Config.static_url_path}/questions_images/{filename}")
+
+            # Вывод сообщения
+            flash("The question has been created", "info")
 
             # Получение ID нового вопроса
             question_id: int = response.json()["id"]
@@ -270,6 +348,9 @@ def edit(question_id: int):
                     # Сохранение изображения
                     image.save(f"{Config.static_url_path}/questions_images/{filename}")
 
+                # Вывод сообщения
+                flash("The question has been edited", "info")
+
                 # Перенос на страницу с отредактированным вопросом
                 return redirect(url_for("question.view", question_id=question_id))
             else:
@@ -326,6 +407,9 @@ def delete(question_id: int):
             if question["image"]:
                 remove_file(f"{Config.static_url_path}/questions_images/{question["image"]}")
 
+            # Вывод сообщения
+            flash("Question deleted", "info")
+
             # Возвращение в меню
             return redirect(url_for("question.home"))
         else:
@@ -375,6 +459,9 @@ def delete_image(question_id: int):
             if response:
                 # Удаление изображения
                 remove_file(f"{Config.static_url_path}/questions_images/{question["image"]}")
+
+                # Вывод сообщения
+                flash("The question image has been deleted", "info")
             else:
                 # Обработка ошибок
                 ResponseErrorHandler.flash_reason_message(response)
